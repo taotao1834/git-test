58109cb2e67155e0998386be9681d1c436d0574a
HJT <791417605@qq.com>
2022-01-17 17:57:04 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author hjt
+ * @Date 2022/1/17 17:51
+ */
+public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
